2bb8f8fe0bb1017933409cc64f73489e16bbd726
Lakshmi <mlakshmi.76@gmail.com>
2022-05-17 10:48:18 -0400

    adding folders

---
+++ lesson3/javascript.js
@@ -0,0 +1,29 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "650px";
+
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "0px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.color = "blue";
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+
+});
+
+
+
+
+
+
